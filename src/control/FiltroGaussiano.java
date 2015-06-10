1bed956dc972ed7c689249600650b331e7bd94c8
Nathan Martins Maia <nathan@linux-mgqm.site>
2015-06-10 09:49:27 -0300

    Muitas dependencias.

--- src/control/FiltroGaussiano.java
+++ src/control/FiltroGaussiano.java
@@ -1,52 +1,51 @@
 package control;
 
-public class FiltroGaussiano {
-
-}
 import java.awt.image.*;
 
 /**
- * A filter which applies Gaussian blur to an image. This is a subclass of ConvolveFilter
- * which simply creates a kernel with a Gaussian distribution for blurring.
+ * A filter which applies Gaussiano  to an image. This is a subclass of ConvolveFilter
+ * which simply creates a kernel with a Gaussiano distribution for ring.
  * @author Jerry Huxtable
  */
-public class GaussianFilter extends ConvolveFilter {
+public class FiltroGaussianoo extends ConvolveFilter {
 
 	static final long serialVersionUID = 5377089073023183684L;
 
-	protected float radius;
+	protected float raio;
 	protected Kernel kernel;
 	
 	/**
-	 * Construct a Gaussian filter
+	 * Construct a Gaussiano filter
 	 */
-	public GaussianFilter() {
+	public FiltroGaussianoo() {
 		this(2);
 	}
 
+	
+
 	/**
-	 * Construct a Gaussian filter
-	 * @param radius blur radius in pixels
+	 * Construct a Gaussiano filter
+	 * @param raio  raio in pixels
 	 */
-	public GaussianFilter(float radius) {
-		setRadius(radius);
+	public FiltroGaussianoo(float raio) {
+		setraio(raio);
 	}
 
 	/**
-	 * Set the radius of the kernel, and hence the amount of blur. The bigger the radius, the longer this filter will take.
-	 * @param radius the radius of the blur in pixels.
+	 * Set the raio of the kernel, and hence the amount of . The bigger the raio, the longer this filter will take.
+	 * @param raio the raio of the  in pixels.
 	 */
-	public void setRadius(float radius) {
-		this.radius = radius;
-		kernel = makeKernel(radius);
+	public void setraio(float raio) {
+		this.raio = raio;
+		kernel = criaKernel(raio);
 	}
 	
 	/**
-	 * Get the radius of the kernel.
-	 * @return the radius
+	 * Get the raio of the kernel.
+	 * @return the raio
 	 */
-	public float getRadius() {
-		return radius;
+	public float getraio() {
+		return raio;
 	}
 
     public BufferedImage filter( BufferedImage src, BufferedImage dst ) {
@@ -60,15 +59,15 @@ public class GaussianFilter extends ConvolveFilter {
         int[] outPixels = new int[width*height];
         src.getRGB( 0, 0, width, height, inPixels, 0, width );
 
-		convolveAndTranspose(kernel, inPixels, outPixels, width, height, alpha, CLAMP_EDGES);
-		convolveAndTranspose(kernel, outPixels, inPixels, height, width, alpha, CLAMP_EDGES);
+		convolucionarETranspor(kernel, inPixels, outPixels, width, height, alpha, CLAMP_EDGES);
+		convolucionarETranspor(kernel, outPixels, inPixels, height, width, alpha, CLAMP_EDGES);
 
         dst.setRGB( 0, 0, width, height, inPixels, 0, width );
         return dst;
     }
 
-	public static void convolveAndTranspose(Kernel kernel, int[] inPixels, int[] outPixels, int width, int height, boolean alpha, int edgeAction) {
-		float[] matrix = kernel.getKernelData( null );
+	public static void convolucionarETranspor(Kernel kernel, int[] inPixels, int[] outPixels, int width, int height, boolean alpha, int edgeAction) {
+		float[] matriz = kernel.getKernelData( null );
 		int cols = kernel.getWidth();
 		int cols2 = cols/2;
 
@@ -79,7 +78,7 @@ public class GaussianFilter extends ConvolveFilter {
 				float r = 0, g = 0, b = 0, a = 0;
 				int moffset = cols2;
 				for (int col = -cols2; col <= cols2; col++) {
-					float f = matrix[moffset+col];
+					float f = matriz[moffset+col];
 
 					if (f != 0) {
 						int ix = x+col;
@@ -112,33 +111,33 @@ public class GaussianFilter extends ConvolveFilter {
 	}
 
 	//Kernel do filtro
-	public static Kernel makeKernel(float radius) {
-		int r = (int)Math.ceil(radius);
+	public static Kernel criaKernel(float raio) {
+		int r = (int)Math.ceil(raio);
 		int rows = r*2+1;
-		float[] matrix = new float[rows];
-		float sigma = radius/3;
+		float[] matriz = new float[rows];
+		float sigma = raio/3;
 		float sigma22 = 2*sigma*sigma;
 		float sigmaPi2 = 2*ImageMath.PI*sigma;
 		float sqrtSigmaPi2 = (float)Math.sqrt(sigmaPi2);
-		float radius2 = radius*radius;
+		float raio2 = raio*raio;
 		float total = 0;
 		int index = 0;
 		for (int row = -r; row <= r; row++) {
-			float distance = row*row;
-			if (distance > radius2)
-				matrix[index] = 0;
+			float distancia = row*row;
+			if (distancia > raio2)
+				matriz[index] = 0;
 			else
-				matrix[index] = (float)Math.exp(-(distance)/sigma22) / sqrtSigmaPi2;
-			total += matrix[index];
+				matriz[index] = (float)Math.exp(-(distancia)/sigma22) / sqrtSigmaPi2;
+			total += matriz[index];
 			index++;
 		}
 		for (int i = 0; i < rows; i++)
-			matrix[i] /= total;
+			matriz[i] /= total;
 
-		return new Kernel(rows, 1, matrix);
+		return new Kernel(rows, 1, matriz);
 	}
 
 	public String toString() {
-		return "Blur/Gaussian Blur...";
+		return "/Gaussiano ...";
 	}
 }
