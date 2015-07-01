47eed5604facf26b89117d5b79a3f53af31c4a05
Nathan Martins Maia <nathan@linux-mgqm.site>
2015-06-30 22:40:12 -0300

    Correção1

--- src/control/ControlePrincipal.java
+++ src/control/ControlePrincipal.java
@@ -4,18 +4,17 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
-
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+import sun.misc.BASE64Encoder;
 import view.principalJanela;
 
 
 public class ControlePrincipal {
 	
 	private BufferedImage dstImg;
-	private ByteArrayOutputStream os = new ByteArrayOutputStream();
+
 	
 	public void startApp(){
 		principalJanela frame = new principalJanela();		
@@ -42,28 +41,20 @@ public class ControlePrincipal {
 	
 	public void aplicaCompressao(){
 		//String strImg = dstImg.toString();
-			    
-	    OutputStream b64 = new Base64.OutputStream(os);
-	    ImageIO.write(dstImg, "png", os);
-	    String strImg = os.toString("UTF-8");
-	    ByteArrayOutputStream out = new ByteArrayOutputStream();
-	    out.writeTo(b64);
-	    String result = out.toString("UTF-8");
-		Compressor cmp = new Compressor();
+		String strImg = imageToString(dstImg);
+	    Compressor cmp = new Compressor();
 		int janela = Integer.parseInt(JOptionPane.showInputDialog(null,"Entre com o tamanho da janela (null ou vazio para tamanho Default: "));
 		String compressedData = cmp.compress(strImg, janela);
 		System.out.println(compressedData);
 	}
 	
-	public String imageToString(BufferedImage bImage, String path)   {
+	public String imageToString(BufferedImage bImage)   {
         String imageString = null;
-
-        String formatName = path.substring(path.lastIndexOf('.')+1, path.length());
 
         //image to bytes
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
-            ImageIO.write(bImage, formatName, baos);
+            ImageIO.write(bImage, "png", baos);
             baos.flush();
             byte[] imageAsRawBytes = baos.toByteArray();
             baos.close();
@@ -74,7 +65,7 @@ public class ControlePrincipal {
             //don't do this!!!
             //imageString = new String(imageAsRawBytes);
         } catch (IOException ex) {
-            Logger.getLogger(ImageToXML.class.getName()).log(Level.SEVERE, null, ex);
+        	throw new RuntimeException("Não foi possível abrir o arquivo!");
         }
 
         return imageString;
